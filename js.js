c34f0edefa192f80dc1e7b6ac1b31a44668de9bd
zyadmagdy99 <zyadmagdy08@gmail.com>
2024-09-25 13:07:45 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,22 @@
+$(".down").click(function () { 
+    $(".list-down").toggle();
+    
+});
+$(".down2").click(function () { 
+    $(".list-down2").toggle();
+    
+});
+$(".down3").click(function () { 
+    $(".list-down3").toggle();
+    
+});
+$(".bars").click(function () { 
+    
+$("nav").addClass("hidden");    
+$(".replace").removeClass("hidden");    
+});
+$(".xx").click(function () { 
+    
+$("nav").removeClass("hidden");    
+$(".replace").addClass("hidden");    
+});
